47aed55500233941637dae8a68e17683c3b6dd90
olvrb <oboudet1@me.com>
2017-10-19 16:42:13 +0200

    initial commit

---
+++ PrgGr6/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrgGr6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] olikaVal = {"Hello", "Hejsan", "Salut" };
+            int val = ConsoleMenu("Menu title", olikaVal);
+            Console.WriteLine("You chose alternative " + (val + 1) + ": " + olikaVal[val]);
+            Console.ReadLine();
+        }
+        static int ConsoleMenu(string titel, string[] options)
+        {
+            bool hasSelectedValue = false;
+            int currentIndex = 0;
+
+            while (!hasSelectedValue)
+            {
+                Console.Clear();
+
+                Console.WriteLine(titel);
+
+                for (int i = 0; i < options.Length; i++)
+                {
+                    if (currentIndex == i)
+                    {
+                        Console.WriteLine(options[i] + " <-");
+                    }
+                    else
+                    {
+                        Console.WriteLine(options[i]);
+                    }
+
+                }
+
+                ConsoleKey key = Console.ReadKey(true).Key;
+
+                if (key == ConsoleKey.UpArrow)
+                {
+                    currentIndex--;
+                    if (currentIndex < 0)
+                    {
+                        currentIndex = 0;
+                    }
+                }
+                else if (key == ConsoleKey.DownArrow)
+                {
+                    currentIndex++;
+                    if (currentIndex >= options.Length)
+                    {
+                        currentIndex = options.Length - 1;
+                    }
+                }
+                else if (key == ConsoleKey.Enter)
+                {
+                    hasSelectedValue = true;
+                }
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            return currentIndex;
+        }
+    }
+}
